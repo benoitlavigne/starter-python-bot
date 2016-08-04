53aea5a2447feaceffa6424a28d0c4dfb6f10969
Benoit Lavigne <benlavigne@MEL-M-BLAVIGNE01.local>
2016-08-04 15:41:18 +1000

    server

--- bot/app.py
+++ bot/app.py
@@ -22,7 +22,7 @@ def local_test():
 
 if __name__ == "__main__":
     print("running")
-    print(os.getenv("PORT"))
+    print(os.getenv("PORT","not found"))
     server.run(host='0.0.0.0',port=7000,debug=True)
     log_level = os.getenv("LOG_LEVEL", "INFO")
     logging.basicConfig(format='%(asctime)s - %(levelname)s: %(message)s', level=log_level)
