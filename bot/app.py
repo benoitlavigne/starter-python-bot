6e8dc3344cd5817aac7a4971e49f82609ed11ed3
Benoit Lavigne <benlavigne@MEL-M-BLAVIGNE01.local>
2016-08-04 15:45:55 +1000

    server

--- bot/app.py
+++ bot/app.py
@@ -22,13 +22,14 @@ logger = logging.getLogger(__name__)
 
 if __name__ == "__main__":
     print("running")
-    print(os.getenv("PORT","not found"))
+    port= os.getenv("PORT","not found")
     #server.run(host='0.0.0.0',port=7000,debug=True)
     log_level = os.getenv("LOG_LEVEL", "INFO")
     logging.basicConfig(format='%(asctime)s - %(levelname)s: %(message)s', level=log_level)
 
     slack_token = os.getenv("SLACK_TOKEN", "")
     logging.info("token: {}".format(slack_token))
+    logging.info(port)
 
     if slack_token == "":
         logging.info("SLACK_TOKEN env var not set, expecting token to be provided by Resourcer events")
